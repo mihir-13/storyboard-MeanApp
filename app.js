f5775abfd021930d93aa61f0cc9ad8ade599005b
Mihir Brahmbhatt <mihirbrahmbhatt3@gmail.com>
2018-05-03 17:28:32 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const app = express();
+
+// Passport config
+require('./config/passport')(passport);
+
+
+
+// Load Routes
+const auth = require('./routes/auth');
+
+// Use routes
+app.use('/auth', auth);
+
 
 
 const port = process.env.PORT || 5000;
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://mihir:mihir@ds053160.mlab.com:53160/storyboards-app',
+    googleClientID: '765673341810-0gi50cnj31qv82b7666nn0gmmbcv4oga.apps.googleusercontent.com',
+    googleClientSecret: 'ijhoR0fGXxRUsx3Psfa24F_6'
+}
