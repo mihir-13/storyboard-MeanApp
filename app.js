783167b0a4bd00fce9ecd5be79de6d23737bad45
Mihir Brahmbhatt <mihirbrahmbhatt3@gmail.com>
2018-05-03 15:47:00 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+res.send("Hello");
+});
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
